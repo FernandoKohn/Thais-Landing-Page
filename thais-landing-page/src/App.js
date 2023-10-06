40ca25ff34669989e916e90de266abacfc06867c
FernandoKohn <contatofernandokohn@gmail.com>
2023-10-05 21:18:36 -0300

    dia 11

--- thais-landing-page/src/App.js
+++ thais-landing-page/src/App.js
@@ -27,13 +27,13 @@ function App() {
       </Router>
       <div className="pagination">
         <div>
-          <a href="https://www.instagram.com/Nixie____"><i class='bx bxl-instagram-alt'></i></a>
+          <a target='_blank' href="https://www.instagram.com/Nixie____"><i class='bx bxl-instagram-alt'></i></a>
         </div>
         <div>
-          <a href="https://www.youtube.com/@Nixienaga/videos"><i class='bx bxl-youtube' ></i></a>
+          <a target='_blank' href="https://www.youtube.com/@Nixienaga/videos"><i class='bx bxl-youtube' ></i></a>
         </div>
         <div>
-          <a href="https://www.tiktok.com/@nixienaga?lang=pt-BR"><i class='bx bxl-tiktok'></i></a>
+          <a target='_blank' href="https://www.tiktok.com/@nixienaga?lang=pt-BR"><i class='bx bxl-tiktok'></i></a>
         </div>
       </div>
 
