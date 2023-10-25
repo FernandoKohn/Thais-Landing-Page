d6091dd8c667f9cf26da463f68b89819ede142bd
FernandoKohn <contatofernandokohn@gmail.com>
2023-10-25 16:22:56 -0300

    dia 22 site terminado
    Devo tudo a meu Senhor  o  único Deus  vivo.

--- thais-landing-page/src/components/Pages/Home.js
+++ thais-landing-page/src/components/Pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
                         <Linha/>
                         <h1>Thais Passos</h1>
                         <p>Streamer - Youtuber - Tiktoker - Influenciadora  </p>
-                        <h2>Com um olho atento para as tendências e um toque de criatividade, meus números nas redes sociais estão sempre em ascensão. Estou em constante busca por novas maneiras de entreter meus seguidores, sempre mantendo um ambiente positivo e acolhedor. Uma criadora de conteúdo apaixonada e cheia de energia, com um sorriso no rosto mergulho de cabeça na criação de conteúdo em diversas plataformas como <span className={styles.links}><a target="_blank" href="https://www.instagram.com/nixie____/">Instagram</a></span>, <spann className={styles.links}><a target="_blank" href="https://www.youtube.com/@Nixienaga">Youtube</a></spann>, <span className={styles.links}><a target="_blank" href="https://www.tiktok.com/@nixienaga?lang=pt-BR">Tiktok</a></span> e <span className={styles.links}><a target="_blank" href="https://www.twitch.tv/nixie_e">Twitch</a></span>.</h2>
+                        <h2>Com um olho atento para as tendências e um toque de criatividade, meus números nas redes sociais estão sempre em ascensão. Estou em constante busca por novas maneiras de entreter meus seguidores, sempre mantendo um ambiente positivo e acolhedor. Uma criadora de conteúdo apaixonada e cheia de energia, com um sorriso no rosto mergulho de cabeça na criação de conteúdo em diversas plataformas como Instagram, Youtube, Tiktok e Twitch.</h2>
                         <div className={styles.ass}></div>                 
                 </div>
             </div>
